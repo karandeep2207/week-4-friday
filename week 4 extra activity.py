129ebb5604b2451ef3d03a3f905d7a5595b4c020
karandeep2207 <160789213+karandeep2207@users.noreply.github.com>
2024-03-15 11:33:52 +1300

    Add files via upload

---
+++ week 4 extra activity.py
@@ -0,0 +1,58 @@
+x=25
+y=10
+# Addition operator(+):
+print(x+y)
+#Subtraction operator (-):
+print(x-y)
+# Multiplication operator (*):
+print(x*y)
+#Division operator (/):
+print(x/y)
+#Modules (Or, Remainder) operator (%)
+print(x%y)
+#Exponentiation operator (**)
+print(y**3)
+#Floor Division (//) operator:
+print(x//y)
+
+
+#Using +=:
+x=5
+x+=2
+print(x)
+#Using-=:
+x-=3
+print(x)
+#Using *=:
+x*=2
+print(x)
+#Using//=:
+x//=2
+print(x)
+
+
+
+print(" result 1 is ",5>2)
+print(" result 2 is ",5<2)
+print(" result 3 is ",20==15+5)
+# Assigning 10 to x, 15 to y and
+# then using comparison operators.
+x = 10
+y = 15
+print(" result 4 is ", x==y)
+print(" result 5 is ", x !=y)
+print(" result 6 is ", x== y-5)
+print(" result 7 is ", x >=y)
+print(" result 8 is ", x <= y-2)
+
+
+print(" result 1 is ", 5>3 and 3>1)
+print(" result 2 is ", 5>3 and 2>4)
+print(" result 3 is ", 5>3 or 2>4)
+print(" result 4 is ", not(5>3))
+print(" result 5 is ", not(2>3))
+print(" result 4 is ", not(5>3 or 2>4))
+x=10
+y=15
+print(" result 7 is ",  x>=10 and y<5)
+print(" result 8 is ",  x>=5 and y>7)
